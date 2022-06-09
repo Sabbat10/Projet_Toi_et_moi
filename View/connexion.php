b202bdb51b31388718cab9d9b5d03636c5df7fa1
sabbat10 <sabbatlumpatshia99@gmail.com>
2022-06-09 10:51:39 +0100

    Le formulaire d'inscription et connexion

---
+++ View/connexion.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <!-- CSS only boodstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Public/design/default.css">
+    <title>Toi et Moi</title>
+</head>
+<body>
+   
+    <header class=" sticky-top text-center">
+        <div class="titre">
+            <h1>To! et Mo!*</h1>
+        </div>
+        <div class="search"></div>
+    </header>
+
+   <div id="#container" class="container align-self-center">
+        <div class="row">
+            <div class="col-lg-3">
+
+            </div>
+            <div class="col-lg-6 col-ms-12">
+                <div class="card p-5">
+                    <h5 class="card-title p-1">Connetez-vous</h5>
+                    <center><img src="../Public/asset/favicon.png" alt="" width="100px" class=""></center>
+                    <form action="" method="post">
+                        <p>
+                            <input type="email" name="" id="" placeholder="Email">
+                        </p>
+                        <p>
+                            <input type="password" id="" name="" placeholder="Mot de passe">
+                        </p>
+                        <p>
+                            <input type="submit" value="Connetez-vous">
+                        </p>
+                        <p>
+                            Si vous n'avez pas encore un compte! <a href="../idex.php">Inscrivez-vous !</a>
+                        </p>
+                    </form>
+                </div>
+            </div>    
+        </div>    
+    </div>
+
+    <footer>
+        <div class="footer">
+                <span class="logo">To! et mo!*</span>
+                <p><a href="https://www.facebook.com/">© Sabbat Lumpatshia</a></p>
+        </div>
+    </footer>
+        
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ idex.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <!-- CSS only boodstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="Public/design/default.css">
+    <title>Toi et Moi</title>
+</head>
+<body>
+   
+    <header class=" sticky-top text-center">
+        <div class="titre">
+            <h1>To! et Mo!*</h1>
+        </div>
+        <div class="search"></div>
+    </header>
+
+    <div id="#container" class="container">
+        <div class="row g-5">
+            <div class="col-lg-7 col-ms-12 align-item-start">
+                <div class="description">
+                    <h2>Description</h2>
+                    <p>
+                        Lorem Ipsum est un générateur de faux textes aléatoires. Vous choisissez le nombre de paragraphes, de mots ou de listes. Vous obtenez alors un texte aléatoire que vous pourrez ensuite utiliser librement dans vos maquettes.
+                        Le texte généré est du pseudo latin et peut donner l'impression d'être du vrai texte.
+                        Lorem Ipsum est un générateur de faux textes aléatoires. Vous choisissez le nombre de paragraphes, de mots ou de listes. Vous obtenez alors un texte aléatoire que vous pourrez ensuite utiliser librement dans vos maquettes.
+                        Le texte généré est du pseudo latin et peut donner l'impression d'être du vrai texte.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-5 col-ms-12">
+                <div class="card">
+                    <h5 class="card-title">Inscrivez-vous</h5>
+                    <center><img src="Public/asset/favicon.png" alt="" width="100px" class=""></center>
+                    <form action="" method="post">
+                        <p>
+                            <input type="text" name="" id="" placeholder="Nom" height="50px" width="30px">
+                        </p>
+                        <p>
+                            <input type="text" name="" id="" placeholder="Prenom">
+                        </p>
+                        <p>
+                            <input type="email" name="" id="" placeholder="Email">
+                        </p>
+                        <p>
+                            <input type="password" id="" name="" placeholder="Mot de passe">
+                        </p>
+                        <p>
+                            <input type="submit" value="Confirmez l'inscription">
+                        </p>
+                        <p>
+                            Si vous avez déjà un compte! <a href="View/connexion.php">Connetez-vous !</a>
+                        </p>
+                    </form>
+                </div>
+            </div>    
+        </div>    
+    </div>
+
+    <footer>
+        <div class="footer">
+                <span class="logo">To! et mo!*</span>
+                <p><a href="https://www.facebook.com/">© Sabbat Lumpatshia</a></p>
+        </div>
+    </footer>
+        
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
